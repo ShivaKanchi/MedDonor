6b27d1b2eeae1c2e817cf44602c59765ab91fd20
Saurabh2482 <saurabh.k.shinde007@gmail.com>
2022-10-12 18:06:08 +0530

    signin remove

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Event from "./Screen/Event";
 import Home from "./Screen/Home";
 import PageNotFound from "./Screen/PageNotFound";
 import Services from "./Screen/Services";
-import SignIn from "./Screen/SignIn";
 import SignUp from "./Screen/SignUp";
 
 function App() {
@@ -22,7 +21,7 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/donner" element={<Donner />} />
           <Route path="/event" element={<Event />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signin"  />
           <Route path="/signup" element={<SignUp />} />
           <Route path="*" element={<PageNotFound />} />
       </Routes>
