de6d824175fe5887617489a1a3ad02d437c00359
saurabh_7582 <saurabh.k.shinde007@gmail.com>
2022-10-31 02:12:04 +0530

    service done

--- src/Screen/Services.js
+++ src/Screen/Services.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Count from '../Component/Count'
+import Footer from '../Component/Footer'
+import GetOffer from '../Component/GetOffer'
 import Head from '../Component/Head'
 import ServicesHead from '../Component/ServicesHead'
+import Work from '../Component/Work'
 
 function Services() {
   return (
     <>
       <Head />
       <ServicesHead />
+      <Work />
+      <Count />
+      <GetOffer />
+      <Footer />
     </>
   )
 }
