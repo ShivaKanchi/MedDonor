5fc0acae84e34b3834b60e9213d6d0c85b4da530
ShivaKanchi <shivakanchi111@gmail.com>
2023-01-13 20:24:53 +0530

    Pull after

--- src/Screen/Event.js
+++ src/Screen/Event.js
@@ -5,8 +5,6 @@ import Count from '../Component/Count'
 import Footer from '../Component/Footer2'
 import EventCard from '../Component/EventCard';
 
-
-
 export default function Event() {
   return (
     <>
