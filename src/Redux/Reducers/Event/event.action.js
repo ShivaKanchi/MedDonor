d5d11159a01f6e0e034c0e07d74b95ac144f42c3
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-06 22:34:13 +0530

    event page edit

--- src/Redux/Reducers/Event/event.action.js
+++ src/Redux/Reducers/Event/event.action.js
@@ -10,11 +10,17 @@ import {
 
 import axios from 'axios';
 
-export const addEvent = () => async () => {
+export const addEvent = (eventData) => async (dispatch) => {
     try {
-
+        await axios({
+            method: "POST",
+            url: `${process.env.REACT_APP_CLIENT_URL}/event/add`,//http://localhost:4000  ${process.env.REACT_APPCLIENT_URL}
+            data: { credentials: eventData }
+        });
+        return dispatch({ type: ADD_EVENT, payload: eventData });
     }
     catch (error) {
+        return dispatch({ type: "ERROR", payload: error });
 
     }
 }
