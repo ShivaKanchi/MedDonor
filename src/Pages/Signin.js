8c92d8470262af85fa7602d0b43afbeac02ae30c
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-08 22:58:37 +0530

    Signout function

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -39,7 +39,7 @@ function SignIn() {
     await dispatch(signIn(data))
     setData({ email: "", password: "" })
     setData({ email: "", password: "" })
-
+    navigate("/")
     console.log("HANDlesub,it", data)
   };
 
