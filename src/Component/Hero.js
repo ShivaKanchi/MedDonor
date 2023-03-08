0ddd46099c1b402e89f450e67a9fc84e8f824f80
saurabh7582 <saurabh.k.shinde007@gmail.com>
2023-03-08 13:41:37 +0530

    traansaction left

--- src/Component/Hero.js
+++ src/Component/Hero.js
@@ -2,49 +2,50 @@ import { Image, Stack, Flex, Text, Box, Heading, Button, Link } from '@chakra-ui
 import React from 'react'
 
 export default function Hero() {
-    
+
   return (
     <Flex>
-        <Box
-            backgroundImage="url(https://res.cloudinary.com/ssdeveloper/image/upload/v1665650294/Med%20Donner/hero_background_smo15x.svg)"
-            backgroundPosition="center"
-            backgroundRepeat="no-repeat"
-            w="full"
-            h="fit-content"
-            zIndex={1}
-            // border="2px solid red"
-        >
-            <Stack direction={['column','row','row']}>
-                <Flex direction="column" w={["100%","50%","50%"]} p="10">
-                    <Image w="26px" h="26px"  align="flex-start" src="https://res.cloudinary.com/ssdeveloper/image/upload/v1665683216/Med%20Donner/Rectangle_3_aajbgu.svg" />
-                        
-                            <Heading 
-                                pl="5" fontWeight="700" fontFamily="IBM Plex Sans" fontStyle="normal"
-                                fontSize={["4xl","5xl","6xl"]} line-height="88px" letterSpacing="0.023em"
-                                >Your Donations<br></br>can make one<br></br>breathe happily
-                            </Heading>
-                    <Text 
-                        p="5" fontWeight="400" fontFamily="IBM Plex Sans" fontStyle="normal"
-                        fontSize="21px" line-height="28px" letterSpacing="0.023em" color="#7C7C7C"
-                    > We're always available for our users with emergency medicine requirements.                    
-                    <br></br>You can easily reach us 24/7. We focused on our user's care.</Text>
-                    <Flex direction="row-reverse">
-                        <Image w="26px" h="26px"   src="https://res.cloudinary.com/ssdeveloper/image/upload/v1665685545/Med%20Donner/Rectangle_4_yafxif.svg" />
+            <Box
+                backgroundImage="url(https://res.cloudinary.com/ssdeveloper/image/upload/v1665650294/Med%20Donner/hero_background_smo15x.svg)"
+                backgroundPosition="center"
+                backgroundRepeat="no-repeat"
+                w="full"
+                h="fit-content"
+                zIndex={1}
+                ref={animatedBoxRef}
+                // border="2px solid red"
+            >
+                <Stack direction={['column','row','row']}>
+                    <Flex direction="column" w={["100%","50%","50%"]} p="10">
+                        <Image w="26px" h="26px"  align="flex-start" src="https://res.cloudinary.com/ssdeveloper/image/upload/v1665683216/Med%20Donner/Rectangle_3_aajbgu.svg" />
+                            
+                                <Heading 
+                                    pl="5" fontWeight="700" fontFamily="IBM Plex Sans" fontStyle="normal"
+                                    fontSize={["4xl","5xl","6xl"]} line-height="88px" letterSpacing="0.023em"
+                                    >Your Donations<br></br>can make one<br></br>breathe happily
+                                </Heading>
+                        <Text 
+                            p="5" fontWeight="400" fontFamily="IBM Plex Sans" fontStyle="normal"
+                            fontSize="21px" line-height="28px" letterSpacing="0.023em" color="#7C7C7C"
+                        > We're always available for our users with emergency medicine requirements.                    
+                        <br></br>You can easily reach us 24/7. We focused on our user's care.</Text>
+                        <Flex direction="row-reverse">
+                            <Image w="26px" h="26px"   src="https://res.cloudinary.com/ssdeveloper/image/upload/v1665685545/Med%20Donner/Rectangle_4_yafxif.svg" />
+                        </Flex>
+                        <Link href='/donnor' w="full" alignSelf="center" >
+                            <Button
+                                size='lg' variant='solid' bg='#20BC7E' _hover="#FFB87A" color="white" transition='all 0.2s cubic-bezier(.08,.52,.52,1)'
+                                borderRadius='47px' w={["80%","50%","40%"]} 
+                            >Find Your Donor
+                            </Button>
+                        </Link>
                     </Flex>
-                    <Link href='/donnor' w="full" alignSelf="center" >
-                        <Button
-                            size='lg' variant='solid' bg='#20BC7E' _hover="#FFB87A" color="white" transition='all 0.2s cubic-bezier(.08,.52,.52,1)'
-                            borderRadius='47px' w={["80%","50%","40%"]} 
-                        >Find Your Donor
-                        </Button>
-                     </Link>
-                </Flex>
-                {/* second half  */}
-                <Flex direction="column" w={["100%","50%","50%"]} >
-                    <Image src="https://res.cloudinary.com/ssdeveloper/image/upload/v1666771237/Med%20Donner/Homepage_Design_2_nxv1ur.svg" />
-                </Flex>
-            </Stack>
-        </Box>
+                    {/* second half  */}
+                    <Flex direction="column" w={["100%","50%","50%"]} >
+                        <Image src="https://res.cloudinary.com/ssdeveloper/image/upload/v1666771237/Med%20Donner/Homepage_Design_2_nxv1ur.svg" />
+                    </Flex>
+                </Stack>
+            </Box>
     </Flex>
     // </Stack>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,39 +11,41 @@ import Hero from '../Component/Hero'
 // import MedicenDonateRegister from '../Component/MedicenDonateRegister'
 import OurServices from '../Component/OurServices'
 
+
 function Home() {
   const user = localStorage.getItem('token')
   console.log(user)
+
   return (
     <>
       <Head />
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <Hero />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <About />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <OurServices />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <BoxSection />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <Count />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <Stack display={["flex","none","none"]}>
           <DonationRegister />
         </Stack>
-      </ScaleFade>
+      {/* </ScaleFade> */}
       {/* <MedicenDonateRegister /> */}
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <GetOffer />
-      </ScaleFade>
-      <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}>
+      {/* </ScaleFade> */}
+      {/* <ScaleFade in={true} direction="bottom" duration={0.3} easing="ease-out" position="fixed" zIndex={1}> */}
         <Footer />
-      </ScaleFade>
+      {/* </ScaleFade> */}
     </>
   )
 }
